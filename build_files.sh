e99c66fd5408e5649bf976812657b16568952c8f
Jacek Ka <j.krawczewski@gmail.com>
2023-10-31 07:38:44 +0100

    avoiding vercel errors

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 pip install --upgrade pip
 pip install -r requirements.txt
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
 python3.9 manage.py collectstatic
 python3.9 manage.py makemigrations
-python3.9 migrate
+python3.9 manage.py migrate
